06c77e942990c5288d8a09c5a07a4644b231ce48
Yash Vasani <vasaniyash9646@gmail.com>
2026-04-13 09:12:46 +0530

    fixed routing using react-router-dom

--- src/MainFiles/main.jsx
+++ src/MainFiles/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../CssParts/index.css'
 import App from './App'
-import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById('root')).render(
-   <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>,
+  </StrictMode>
 )
